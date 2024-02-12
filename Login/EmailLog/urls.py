c0fe3019516477094efc4beb02feecff28c51b86
Rakesh sethy <130262466+1234Rakesh@users.noreply.github.com>
2024-02-12 11:25:19 +0530

    Add files via upload
    authentication

---
+++ Login/EmailLog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from EmailLog.views import UserRegistrationView,UserLoginView
+urlpatterns = [
+     path('register/', UserRegistrationView.as_view(), name='register'),
+     path('login/',UserLoginView.as_view(),name='Login')
+]
